656fde412018cdd08ec07d7fe233fe81bc8a1275
Shipra Agarwal <ssinghals073@gmail.com>
2024-06-22 15:18:06 +0530

    Updates

---
+++ precache-manifest.b693fce612a5c0f9304c7c33bbf2ced9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26d8866339301ff593e16a30459cafc",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "9eea1707d1dd7e049248",
+    "url": "/react-project/static/css/main.abea91a2.chunk.css"
+  },
+  {
+    "revision": "aef350a8ce671fdfc2db",
+    "url": "/react-project/static/js/2.2b682cfe.chunk.js"
+  },
+  {
+    "revision": "0742948f272a60bdf22d264c3e9be5d5",
+    "url": "/react-project/static/js/2.2b682cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eea1707d1dd7e049248",
+    "url": "/react-project/static/js/main.e23e8ace.chunk.js"
+  },
+  {
+    "revision": "474d709649ec7bd064c5",
+    "url": "/react-project/static/js/runtime-main.c4c46e17.js"
+  },
+  {
+    "revision": "c71a3e25c514074230adcebdfed20b7f",
+    "url": "/react-project/static/media/img1.c71a3e25.jpg"
+  },
+  {
+    "revision": "e2f8bd6e421b8185ab2513f9226e7f0b",
+    "url": "/react-project/static/media/img2.e2f8bd6e.jpg"
+  },
+  {
+    "revision": "771d4f2e268e46f43f45ac57731b4795",
+    "url": "/react-project/static/media/img3.771d4f2e.jpg"
+  },
+  {
+    "revision": "f052a3a5e7ab4bffae33505ac735482b",
+    "url": "/react-project/static/media/img4.f052a3a5.jpg"
+  },
+  {
+    "revision": "c6529f2b386918f52e5be603e28c3e6a",
+    "url": "/react-project/static/media/img5.c6529f2b.jpg"
+  },
+  {
+    "revision": "7c34da55b02b723505428eb1491fa4ed",
+    "url": "/react-project/static/media/img6.7c34da55.jpg"
+  }
+]);
